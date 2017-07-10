ce216310ef217586d55ef6ed645e654335771202
rnemykin <nemykin.roman@gmail.com>
2017-07-10 14:49:34 +0300

    init

---
+++ src/main/java/ru/homer/leaderboard/LeaderBoardApplication.java
@@ -0,0 +1,12 @@
+package ru.homer.leaderboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaderBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaderBoardApplication.class, args);
+	}
+}
