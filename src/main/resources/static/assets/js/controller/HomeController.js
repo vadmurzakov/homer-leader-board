699e158270ef86babadafc67bcb5c95ef6bc093b
vadmurzakov <vadmurzakov@gmail.com>
2017-07-23 17:14:06 +0300

    ~ html-statistics

--- src/main/resources/static/assets/js/controller/HomeController.js
+++ src/main/resources/static/assets/js/controller/HomeController.js
@@ -4,29 +4,37 @@
 LeaderBoard.controller('HomeController', ['$scope', '$http', '$rootScope', function ($scope, $http, $rootScope) {
     console.log('HomeController');
 
-    $scope.progressBar = 0;
+    $scope.progressBarShow = true;
     $scope.tabs = [];
 
     var HOST_URI = /^(https?:\/\/)?([\da-z\.-:]+)+/.exec(window.location.href)[0];
 
-    users = [
-        {username: 'vmurzakov', fullname: 'Мурзаков Вадим'},
-        {username: 'vuvarov', fullname: 'Уваров Владимир'},
-        {username: 'rnemykin', fullname: 'Немыкин Рома'},
-        {username: 'ilysenko', fullname: 'Илья Лысенко'}
-    ];
+    // users = [
+    //     {username: 'vmurzakov', fullname: 'Мурзаков Вадим'},
+    //     {username: 'vuvarov', fullname: 'Уваров Владимир'},
+    //     {username: 'rnemykin', fullname: 'Немыкин Рома'},
+    //     {username: 'ilysenko', fullname: 'Илья Лысенко'}
+    // ];
+    //
+    // users.forEach(function (item) {
+    //     $http.get(HOST_URI + '/api/v1/issue/' + item.username + '/month/1')
+    //         .then(function onSuccess(response) {
+    //             $scope.tabs.push({
+    //                 title: item.fullname,
+    //                 issues: response.data
+    //             });
+    //             $scope.progressBar += 100 / users.length;
+    //         }, function onError(response) {
+    //             console.error(response);
+    //         });
+    // });
 
-    users.forEach(function (item) {
-        $http.get(HOST_URI + '/api/v1/issue/' + item.username + '/month/1')
-            .then(function onSuccess(response) {
-                $scope.tabs.push({
-                    title: item.fullname,
-                    issues: response.data
-                });
-                $scope.progressBar += 100 / users.length;
-            }, function onError(response) {
-                console.error(response);
-            });
-    });
+    $http.get(HOST_URI + '/api/v1/statistic/')
+        .then(function onSuccess(response) {
+            $scope.statistics = response.data;
+            $scope.progressBarShow = false;
+        }, function onError (response) {
+            console.error(response);
+        });
 
 }]);
