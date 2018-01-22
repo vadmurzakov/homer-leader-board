ef87ec6f4b57937d8578af798cd8222eae3ab01a
vadmurzakov <vadmurzakov@gmail.com>
2018-01-22 13:27:19 +0300

    optional analytic, patch and capacity

--- src/main/resources/static/assets/js/controller/HomeController.js
+++ src/main/resources/static/assets/js/controller/HomeController.js
@@ -10,6 +10,19 @@ LeaderBoard.controller('HomeController', ['$scope', '$http', '$rootScope', funct
     $scope.isProgressBarShow = true;
     $scope.countMonth = 6;
 
+    $scope.capacity = {
+        isShow: false,
+        button: 'danger'
+    };
+    $scope.analytic = {
+        isShow: false,
+        button: 'danger'
+    };
+    $scope.patch = {
+        isShow: false,
+        button: 'danger'
+    };
+
     var HOST_URI = /^(https?:\/\/)?([\da-z0-9\.\-:]+)/.exec(window.location.href)[0];
 
     usernames = [
@@ -34,12 +47,27 @@ LeaderBoard.controller('HomeController', ['$scope', '$http', '$rootScope', funct
             });
     });
 
-    $scope.propertyName = 'fullname';
+    // $scope.propertyName = 'fullname';
     $scope.reverse = true;
 
     $scope.sortBy = function(propertyName) {
         $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
         $scope.propertyName = propertyName;
     };
+
+    $scope.showBy = function(columnName) {
+        if (columnName === 'capacity') {
+            $scope.capacity.isShow = !$scope.capacity.isShow;
+            $scope.capacity.button = $scope.capacity.isShow ? 'success' : 'danger';
+        }
+        if (columnName === 'analytic') {
+            $scope.analytic.isShow = !$scope.analytic.isShow;
+            $scope.analytic.button = $scope.analytic.isShow ? 'success' : 'danger';
+        }
+        if (columnName === 'patch') {
+            $scope.patch.isShow = !$scope.patch.isShow;
+            $scope.patch.button = $scope.patch.isShow ? 'success' : 'danger';
+        }
+    }
 
 }]);
